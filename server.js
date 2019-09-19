2256eea84c7e9507c1185880d7e4462adfb6e1cd
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-09-18 22:08:10 -0500

    Fetch data from backend

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const app = express();
 const PORT = 3000;process.env.PORT || 3000
 //Serving Static file to the browser
 app.use(express.static(__dirname))
+
+app.get('/messages', (req, res) => {
+     const message = [
+          {"name": "Adeyemi", "message": "I am here to talk"},
+          {"name": "Vicky", "message": "I am also here"}
+     ]
+     res.send(message )
+})
+
+
+
 //Listen to server
 app.listen(PORT, ()=>{
      console.log(`Server listen to port ${PORT}`)
