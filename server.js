bc8df7417a2af48523493f47696c24f0f79af54b
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-09-19 12:08:05 -0500

    Modified the post message

--- server.js
+++ server.js
@@ -5,24 +5,39 @@ require("dotenv").config();
 const express = require('express');
 const axios = require('axios')
 const moment = require('moment')
-
-
+const bodyparser = require('body-parser')
 const app = express();
+
 //Set Port Number
 const PORT = 3000;process.env.PORT || 3000
+
 //Serving Static file to the browser
 app.use(express.static(__dirname))
 
+//Extract request data from stream
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:false}))
+
+//Serve messages to the client4
+const messages = []
+
 app.get('/messages', (req, res) => {
-     const message = [
-          {"name": "Adeyemi", "message": "I am here to talk"},
-          {"name": "Vicky", "message": "I am also here"}
-     ]
-     res.send(message )
+     
+     res.send(messages )
 })
 
 
+// Get messages from the client
+app.post('/messages', (req, res) => {
+     let message = req.body;
+     if(message.name  && message.message){
+          messages.push(message)
+          res.sendStatus(200)
+     }    
 
+})
+
+console.log(messages.length)
 //Listen to server
 app.listen(PORT, ()=>{
      console.log(`Server listen to port ${PORT}`)
